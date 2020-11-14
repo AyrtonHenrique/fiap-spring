e583eaeda1eb5437a087c27c74c3c7f9e009a181
saraPiresDev <sararegina.pires@gmail.com>
2020-11-14 12:11:17 -0300

    - Classe de configuração do JPA.

---
+++ ClienteAluno/src/main/java/br/com/fiapspring/config/JpaConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.fiapspring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author SaraRegina
+ *
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
